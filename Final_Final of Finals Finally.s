5779d9557d6fe3557c1a5b055044d8ff0c712631
sccheezum <138411502+sccheezum@users.noreply.github.com>
2024-05-01 20:14:20 -0400

    Update Final_Final of Finals Finally.s
    I changed the start to have the turn dynamics from 3.3 bc it will be very hard to try to change it to not be like that. also put in uppercase comments that I think I can't address wo your code tho I might be able to now? idk my brain is seizing looking at this lol

--- Final_Final of Finals Finally.s
+++ Final_Final of Finals Finally.s
@@ -1,5 +1,15 @@
 ; TODO: 
 
+p_sum: db 0x14      ; keeping track of current player card sum / score
+c_sum: db 0x15      ; these values are currently assigned due to the IPA's isolation
+
+p_bet: db 0x00      ; player & computer bet stores
+c_bet: db 0x00
+c_bet_mode: db 0x02 ; computer betting mode (0 - conservative, 1 - normal, 2 - aggressive)
+p_total: dw 0x00c8  ; total amount of money available for the player and computer
+c_total: dw 0x00c8  ; 200 for testing purposes
+p_wins: db 0x00     ; total wins for player and computer
+c_wins: db 0x00
 
 ; Universal Procedures:
 def convertValToString{
@@ -48,6 +58,32 @@ def askForNumDecks {
 ; Otherwise, jumps to invalid_deck_selection
 	ret
 }
+; add in three mode definitions
+def _conservative{ ; 20% smaller bet from computer
+    mov al, byte p_bet
+    mov bl, 0x04
+    mul bl
+    mov bl, 0x05
+    div bl
+    mov byte c_bet, al
+    ret
+}
+
+def _normal{ ; computer bet matches player
+    mov al,  byte p_bet
+    mov byte c_bet, al
+    ret
+}
+
+def _aggressive{ ; 30% larger bet from computer
+    mov al, byte p_bet
+    mov bl, 0x0d
+    mul bl
+    mov bl, 0x0a
+    div bl
+    mov byte c_bet, al
+    ret
+}
 
 def askForCompBetMode {
 ; User can choose between three computer betting modes:
@@ -58,6 +94,7 @@ def askForCompBetMode {
 ; Otherwise, jumps to invalid_comp_bet_mode
 	ret
 }
+
 
 def askForCompRiskLevel {
 ; User can choose between three computer risk levels:
@@ -126,6 +163,82 @@ def askUserForNextTurn {
 
 
 start:
+    mov al, byte p_bet
+    mov bl, byte c_bet
+    add al, 0x32       ; player bets 50 (NEED TO CHANGE VALUE TO INTERACT WITH USER)
+    mov byte p_bet, al
+    cmp byte c_bet_mode, 0x01 ; comparing to determine what bet mode comp is in
+    jl _call_conservative 
+    je _call_normal
+    jg _call_aggressive
+_continue:
+    mov al, byte p_sum
+    mov bl, byte c_sum
+    cmp al, 0x15 ; testing to see if player has reached 21
+    je _p_win
+    jg _c_win
+    cmp bl, 0x15
+    je _c_win
+    jg _p_win
+    cmp al, bl
+    je _tie
+    jg _p_win
+    jl _c_win
+
+_tie:
+    jmp _end
+    ; Display to the user that the round ended in a tie so no one won
+    ; (to be completed later with the rest of the front end work)
+
+_p_win:
+    mov al, byte p_wins
+    inc al
+    mov byte p_wins, al
+    mov al, byte c_bet
+    mov bl, byte p_total
+    mov cl, byte c_total
+    add bl, al
+    sub cl, al
+    cmp cl, 0x00
+    jle _p_game_win      ; if the computer has no money left, player wins
+    mov byte p_total, bl
+    mov byte c_total, cl
+    jmp _end
+
+_c_win:
+    mov al, byte c_wins
+    inc al
+    mov byte c_wins, al
+    mov al, byte p_bet
+    mov bl, byte c_total
+    mov cl, byte p_total
+    add bl, al
+    sub cl, al
+    cmp cl, 0x00
+    jle _c_game_win   ; if the player has no money left, computer wins
+    mov byte c_total, bl
+    mov byte p_total, cl
+    jmp _end
+
+_end:
+    ; if ending due to no more cards or players choice then compare total wins
+    ; If losing a bet results in no money then the player with money remaining wins 
+    mov al, byte p_wins
+    mov bl, byte c_wins
+    cmp al, bl
+    jg _p_game_win
+    jl _c_game_win
+    
+_p_game_win:
+    ; display that the player won
+    mov al, 0x11            ; placeholder val for testing (NEED TO CHANGE W/ USER INTERACT)
+
+_c_game_win:
+    ; Display that the computer won
+    mov al, 0xcc            ; placeholder val for testing (NEED TO CHANGE W/ USER INTERACT)
+
+    
+    
 ; Initializes Key Variables used
 
 ; User Configuration:
